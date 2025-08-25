2bc2b657ad44f8d0e4952bda269c92afd1c8fc66
KOUSIK MUKHERJEE <mukherjeekousik27@gmail.com>
2025-08-25 12:55:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+        // 1 st qusation odd number
+// const OddOrEven = require("./sunday/odd");
+// console.log(OddOrEven(13)); 
+// console.log(OddOrEven(13)); 
+// console.log(OddOrEven(20));
+// console.log(OddOrEven(5));
+         //2nd qusation loop
+// const evennumber=require("./sunday/2loop");
+// evennumber();
+             //3rd odd number
+//   const OddNumbers = require("./sunday/3odd");
+// OddNumbers();
+            //4th qusation
+        //     const Prime = require("./sunday/prime");
+        //     console.log(prime(11));
+        //     console.log(Prime(10));
+        //      6 quasation
+//         const largest = require("./sunday/6array");
+// console.log(largest([5,2,10, 25, 5, 27,100,100.1])); 
+                 //  7 qusation
+// const smallest = require("./sunday/7arry");
+// console.log(smallest([10, 25, 5, 27])); 
+                 
+                //  13 qusation
+ 
+// const factorial = require('./sunday/13factorial');
+// console.log(factorial(5));   
+// console.log(factorial(6));  
+          // 18 disable
+//           const disable = require("./sunday/disiable");
+//                 disable();
+//       11 qusation
+// const reverse = require("./sunday/11arrayloop");
+// console.log(reverse([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30])); 
---
+++ index.js
@@ -0,0 +1,53 @@
+// // const factorial = require('./sunday');
+
+// // console.log(factorial(5));   /
+// // console.log(factorial(7));  
+// // const factorial = require('./sunday/13factorial');
+//             // 13
+// // console.log(factorial(5));   
+// // console.log(factorial(6));   
+//     //  14 qusation
+//     // const fibonacci=require("./sunday/fibonacci");
+//         // 16 qusation
+        
+// // const array = require("./sunday/loop");
+// // console.log(array([1, 2, 3, 4]));    
+// // console.log(array([5, 6, 7]));       
+// // console.log(array([1000, 50, 1, 0]));  
+
+
+// // const oddddd = require("./sunday/20odd");
+
+// // console.log("The first 20 odd numbers :");
+// // oddddd();
+//              // 23 qusation
+
+// // const Even = require("./sunday/20even");
+// // console.log("The first 20 even numbers:");
+// // Even();
+//                  //  29 qusation
+//                  // testPrimeFilter.js
+
+// // const Primes = require("./sunday/29prime");
+
+// // const arr = [4,7,6,11,50,51,15,32,23,25];
+
+// // Primes(arr);
+// // 12 qusation
+
+// const number = require("./sunday/12array");
+// const myArray = [4, 8, 12, 25, 42, 99];
+// console.log("4 in the array?");
+// console.log(number(myArray, 25)); 
+// console.log(" 7  in the array?");
+// console.log(number(myArray, 7)); 
+//       27 qusation
+// const OddEven = require('./sunday/27two array');
+// const numbers = [1,2, 3, 4,5,6, 7, 9, 12,15,20,21,34,1000,1233,];
+// const result = OddEven(numbers);
+
+// console.log("Odd:", result.odd);   
+// console.log("Even:", result.even); 
+// 17 qusation
+const Table = require('./sunday/17multi');
+Table(100);
